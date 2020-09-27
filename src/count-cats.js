a43558cc0efc4d2bfbc40f067c43c92903f3a8c9
pashaeryshken <pashaeryshken64@gmail.com>
2020-09-28 02:09:25 +0300

    feat: count-cats dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(arr) {
+
+  return arr.flat().reduce((sum ,item ) => {
+    return sum += item === '^^' ? 1 : 0
+  }, 0);
+
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(arr) {
+  try {
+    const string = arr.reduce((string, item) => {
+      if (typeof item === 'string') {
+        return string += item.trim()[0]
+      } else {
+        return string
+      }
+    }, '');
+    if (string) {
+      return string.toUpperCase().split('').sort().join('');
+    }
+    throw new CustomError('Not implemented');
+  } catch (e) {
+    return false
+  }
+
   // remove line with error and write your code here
 };
